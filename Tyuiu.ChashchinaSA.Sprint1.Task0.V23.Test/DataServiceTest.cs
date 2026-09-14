2c86369872bb825d37dc4b2e43ead74be91d0cca
Соня Чащина <tchashinasonja@yandex.ru>
2026-09-14 14:12:45 +0500

    Add project files.

---
+++ Tyuiu.ChashchinaSA.Sprint1.Task0.V23.Test/DataServiceTest.cs
@@ -0,0 +1,15 @@
+﻿using Tyuiu.ChashchinaSA.Sprint1.Task0.V23.Lib;
+namespace Tyuiu.ChashchinaSA.Sprint1.Task0.V23.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            DataService ds = new DataService();
+            var res = ds.Calculate();
+            Assert.AreEqual(13, res);
+        }
+    }
+}
---
+++ Tyuiu.ChashchinaSA.Sprint1.Task0.V23/Program.cs
@@ -0,0 +1,35 @@
+﻿using Tyuiu.ChashchinaSA.Sprint1.Task0.V23.Lib;
+namespace Tyuiu.ChashchinaSA.Sprint1.Task0.V23
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #1 | Выполнила: Чащина С. А. | АСОиУб-26-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #1                                                               *");
+            Console.WriteLine("* Тема: Базовые навыки работы в C#                                        *");
+            Console.WriteLine("* Задание #0                                                              *");
+            Console.WriteLine("* Вариант #0                                                              *");
+            Console.WriteLine("* Выполнила: Чащина Софья Алексеевна | АСОиУб-26-1                        *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу, которая вычисляет выражение 10 / (2 + 3)            *");
+            Console.WriteLine("* и печатает результат на экране.                                         *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("*ИСХОДНЫЕ ДАННЫЕ:                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* (15 / 5 * 4) + 1                                                        *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine(ds.Calculate());
+
+            Console.ReadLine();
+        }
+    }
+}
