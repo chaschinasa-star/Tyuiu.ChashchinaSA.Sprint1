b86b893962fc248b2e053cf13852f52c3d0ef3a8
Соня Чащина <tchashinasonja@yandex.ru>
2026-09-14 15:41:08 +0500

    new lib

---
+++ Tyuiu.ChashchinaSA.Sprint1.Task0.Lib/DataService.cs
@@ -0,0 +1,11 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint1;
+namespace Tyuiu.ChashchinaSA.Sprint1.Task0.Lib
+{
+    public class DataService : ISprint1Task0V23
+    {
+        public double Calculate()
+        {
+            return (15 / 5 * 4) + 1;
+        }
+    }
+}
