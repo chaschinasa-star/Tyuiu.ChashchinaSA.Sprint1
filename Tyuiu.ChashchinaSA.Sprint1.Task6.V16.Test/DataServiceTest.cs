c057b2da87c92b16b8d453e162b8a77cd4068452
Соня Чащина <tchashinasonja@yandex.ru>
2026-09-21 19:31:04 +0500

    Task6

--- Tyuiu.ChashchinaSA.Sprint1.Task6.V16.Test/DataServiceTest.cs
+++ Tyuiu.ChashchinaSA.Sprint1.Task6.V16.Test/DataServiceTest.cs
@@ -4,52 +4,14 @@ namespace Tyuiu.ChashchinaSA.Sprint1.Task6.V16.Test
     [TestClass]
     public sealed class DataServiceTest
     {
-        [TestClass]
-        public class DataServiceTests
+        [TestMethod]
+        public void ValidString()
         {
-            [TestMethod]
-            public void ValidString()
-            {
-                string strTest = "1 ! 2 ? 3";
-                DataService ds = new DataService();
+            DataService ds = new DataService();
+            string strTest = "1 ! 2 ? 3";
+            bool res = ds.CheckSpecSymbols(strTest);
 
-                bool res = ds.CheckSpecSymbols(strTest);
-
-                Assert.IsTrue(res);
-            }
-
-            [TestMethod]
-            public void ValidOnlyExclamation()
-            {
-                string strTest = "Только восклицание!";
-                DataService ds = new DataService();
-
-                bool res = ds.CheckSpecSymbols(strTest);
-
-                Assert.IsFalse(res);
-            }
-
-            [TestMethod]
-            public void ValidOnlyQuestion()
-            {
-                string strTest = "Только вопрос?";
-                DataService ds = new DataService();
-
-                bool res = ds.CheckSpecSymbols(strTest);
-
-                Assert.IsFalse(res);
-            }
-
-            [TestMethod]
-            public void ValidNoSymbols()
-            {
-                string strTest = "Простой текст без знаков";
-                DataService ds = new DataService();
-
-                bool res = ds.CheckSpecSymbols(strTest);
-
-                Assert.IsFalse(res);
-            }
+            Assert.IsTrue(res);
         }
     }
 }
